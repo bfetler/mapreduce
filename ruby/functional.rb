c1e5eb8e50f486f258aaf2254d64e2fed828334e
BFetler <bfetler.gmail.com>
2016-01-12 23:41:59 -0500

    clean up questions

--- ruby/functional.rb
+++ ruby/functional.rb
@@ -3,14 +3,14 @@
 puts 'simple functional map-reduce examples'
 
 arr = [5, 10, 15, 20, 25]
-puts 'input array     : ' + arr.to_s
+puts "input array     : " + arr.to_s
 
 # anonymous methods
 puts "\nuse procs, anonymous methods"
 
 # map mult by 2
 a2 = arr.map { |a| a*2 }
-puts 'map: mult by 2  : ' + a2.to_s
+puts "map: mult by 2  : " + a2.to_s
 
 @big = 12
 
@@ -20,10 +20,10 @@ puts "filter: if > #{@big} : " + a2.to_s
 
 # reduce sum
 a2 = arr.reduce { |a,b| a + b }
-puts 'reduce: sum     : ' + a2.to_s
+puts "reduce: sum     : " + a2.to_s
 
 a2 = arr.reduce(:+)
-puts 'reduce: sum+    : ' + a2.to_s
+puts "reduce: sum+    : " + a2.to_s
 
 
 # wrap code in named methods, keep code DRY, reusable
@@ -41,45 +41,57 @@ end
 # reduce returns single value
 def sumArray(a1)
   return a1.reduce { |a, b| a + b }
-# return a1.reduce(:+)
 end
 
 puts "\nuse named methods for code reuse"
-puts 'map: mult by 2  : ' +  multBy2(arr).to_s
+puts "map: mult by 2  : " +  multBy2(arr).to_s
 puts "filter: if > #{@big} : " + filterBig(arr).to_s
-puts 'reduce: sum     : ' + sumArray(arr).to_s
+puts "reduce: sum     : " + sumArray(arr).to_s
 
 
 # fun with ruby
 puts "\nfun with map filter reduce"
 
-puts 'bang methods replace input variable, not pure FP'
+puts "bang methods replace input variable, not pure FP"
 
 # map mult by 2
 arr.map! { |a| rand(30) }.sort!
-puts 'input array     : ' + arr.to_s
+puts "input array      : " + arr.to_s
 
 arr.map! { |a| a*2 }
-puts 'map!: mult by 2  : ' + arr.to_s
+puts "map!: mult by 2  : " + arr.to_s
 
 # filter if > 24
 arr.select! { |a| a > 24 }
-puts 'select!: if > 24 : ' + arr.to_s
+puts "select!: if > 24 : " + arr.to_s
 
 # reduce sum
 begin
-# reduce! method does not exist
+# reduce! method does not exist.  
 a2 = arr.reduce! { |a,b| a + b }
 rescue NoMethodError => e
   puts e.message
 end
 
-# Questions
-# What kind of method (map, filter, reduce) is:
-#    Array.reject() ?
-#    Array.join() ?
-#    Array.find() ?
-#    Array.zip() ?
+bowie = ['turn', 'and', 'face', 'the', 'strange', 'changes']
+changes = (bowie[-1][0..1]+'-') * 4 + bowie[-1]
+puts "\nbowie says: " + changes
+puts bowie.map { |b| b[0..1] + '-' + b }.to_s
 
+# Questions.
+#
+# 1. Why does Array.reduce! not exist?
+#
+# 2. How would you implement map, select, reduce?  Hint: see Enumerator
+#
+# 3. Are the following methods similar to map, filter, or reduce?
+#    Or combinations of them?  How would you best describe them?
+#        Array.reject() ?
+#        Array.join() ?
+#        Array.find() ?
+#        Array.zip() ?
+#
+# 4. For all the methods in Array.methods.sort, how many are similar to
+#    map, filter, reduce?
 
 
