6eadb2188c1e0c5265ed4ac3ef8532f51cc84f90
BFetler <bfetler.gmail.com>
2016-01-16 11:01:26 -0500

    clean up comments

--- ruby/imperative.rb
+++ ruby/imperative.rb
@@ -5,25 +5,27 @@ puts 'simple imperative map-reduce examples'
 arr = [5, 10, 15, 20, 25]
 puts 'input array     : ' + arr.to_s
 
-# anonymous methods
-puts "\nuse procs, anonymous methods"
+# blocks ~ anonymous methods
+puts "\nuse blocks, similar to anonymous methods"
 
 # map multiply by 2
 a2 = []
 arr.each{ |a| a2 << a*2 }  # push element on array
-puts 'map: mult by 2  : ' + a2.to_s
+puts "map: mult by 2  : " + a2.to_s
+
+@big = 12
 
 # filter if > 12
 a2 = []
 arr.each{ |a|
-  if (a > 12) then a2 << a end
+  if (a > @big) then a2 << a end
 }
-puts 'filter: if > 12 : ' + a2.to_s
+puts "filter: if > #{@big} : " + a2.to_s
 
 # reduce sum
 a2 = 0
 arr.each{ |a| a2 += a }
-puts 'reduce: sum     : ' + a2.to_s
+puts "reduce: sum     : " + a2.to_s
 
 
 # wrap code in named methods, keep code DRY, reusable
@@ -35,14 +37,11 @@ def multBy2(a1)
   return a2
 end
 
-# big = 12
-
 # filter uses boolean
 def filterBig(a1)
-  big = 12
   a2 = []
   a1.each{ |a|
-    if (a > big) then a2 << a end
+    if (a > @big) then a2 << a end
   }
   return a2
 end
@@ -55,7 +54,7 @@ def sumArray(a1)
 end
 
 puts "\nuse named methods for code reuse"
-puts 'map: mult by 2  : ' +  multBy2(arr).to_s
-puts 'filter: if > 12 : ' + filterBig(arr).to_s
-puts 'reduce: sum     : ' + sumArray(arr).to_s
+puts "map: mult by 2  : " +  multBy2(arr).to_s
+puts "filter: if > #{@big} : " + filterBig(arr).to_s
+puts "reduce: sum     : " + sumArray(arr).to_s
 
