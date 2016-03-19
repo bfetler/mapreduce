607af99b027a600c1e6260e4d685242ff1cdd95f
BFetler <bfetler.gmail.com>
2016-03-19 14:25:33 -0400

    update C speed

--- python/loops.c
+++ python/loops.c
@@ -9,26 +9,45 @@
 
 #include <stdio.h>
 #include <sys/time.h>
+#include <math.h>
 
+#define LOOPS 20
 #define MAX 1000000
+
+long int get_timediff(struct timeval t1, struct timeval t2) {
+  long int diff = (t2.tv_sec - t1.tv_sec)*1000000L + t2.tv_usec - t1.tv_usec;
+  printf(" %ld", diff);
+  return diff;
+}
 
 int main(int argc, char **argv) {
   struct timeval t1 = { 0 }, t2 = { 0 };
 
+  long int delta[LOOPS], avg=0;
+  double sigma=0.0;
   int arr[MAX];
 
-  printf("loop count %d  ", MAX);
-  for (int i=0; i<MAX; i++) {
-    arr[i] = i;
-  }
+  printf("array size %d, delta times (us):", MAX);
+  for (int j=0; j<LOOPS; j++) {
+    for (int i=0; i<MAX; i++) {
+      arr[i] = i;
+    }
 
-  gettimeofday(&t1, NULL);
-  for (int i=0; i<MAX; i++) {
-    arr[i] *= 2;
-  }
-  gettimeofday(&t2, NULL);
+    gettimeofday(&t1, NULL);
+    for (int i=0; i<MAX; i++) {
+      arr[i] *= 2;
+    }
+    gettimeofday(&t2, NULL);
 
-  printf("  delta time: %ld us\n", (t2.tv_sec - t1.tv_sec)*1000000L + t2.tv_usec - t1.tv_usec);
+    delta[j] = get_timediff(t1, t2);
+  }
+  for (int j=0; j<LOOPS; j++) {
+    avg += delta[j];
+    sigma += (double) (delta[j] * delta[j]);
+  }
+  avg /= LOOPS;
+  sigma = sqrt(sigma) / (double)LOOPS;
+  printf("\n  time average: %ld +- %.1f us\n", avg, sigma);
 
   return 0;
 }
